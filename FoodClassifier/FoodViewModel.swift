43db67e2dca8ca4b007c60bd7dd5694f77462a55
Yuri Goncharov <yurig@qualtie.com>
2021-09-09 15:52:40 +0300

    fixed minor issue

--- FoodClassifier/FoodViewModel.swift
+++ FoodClassifier/FoodViewModel.swift
@@ -4,13 +4,13 @@ class FoodViewModel: ObservableObject {
     let images = ["1", "2", "3", "4"]
     
     @Published var isUpdatedModelAvailable: Bool = false
-    @Published var useUpdatedModel: Bool = true
+    @Published var useUpdatedModel: Bool = false
     
     private let classifier = Classifier()
     private let downloader = ModelDownloader()
     
     private var modelURL: URL {
-        isUpdatedModelAvailable ? downloader.modelURL : FoodV1.urlOfModelInThisBundle
+        useUpdatedModel ? downloader.modelURL : FoodV1.urlOfModelInThisBundle
     }
     
     func classify(image: String) -> String {
