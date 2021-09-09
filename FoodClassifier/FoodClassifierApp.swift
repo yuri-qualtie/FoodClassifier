52081bc6644b8e46c2fb23c88b148378289368d0
Yuri Goncharov <yurig@qualtie.com>
2021-09-09 14:27:52 +0300

    Initial Commit

---
+++ FoodClassifier/FoodClassifierApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodClassifierApp.swift
+//  FoodClassifier
+//
+//  Created by Yuri Goncharov on 09.09.2021.
+//
+
+import SwiftUI
+
+@main
+struct FoodClassifierApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
